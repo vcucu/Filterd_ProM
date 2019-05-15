a6cc988a1b27ce8a5d3de7d8a27d986106562cca
cldme <ioncld97@gmail.com>
2019-05-15 14:24:37 +0200

    Added some basic logic for the notebook UI

---
+++ src/org/processmining/filterd/gui/AddCellController.java
@@ -0,0 +1,39 @@
+package org.processmining.filterd.gui;
+
+import java.io.IOException;
+
+import org.deckfour.xes.model.XLog;
+import org.processmining.contexts.uitopia.UIPluginContext;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class AddCellController {
+	
+	private NotebookModel model;
+	private UIPluginContext context;
+	private XLog log;
+	private VBox layout;
+	
+	@FXML private Button addComputationCell;
+	@FXML private Button addTextCell;
+	
+	public AddCellController(NotebookModel model, VBox layout) {
+		this.model = model;
+		this.layout = layout;
+	}
+	
+	@FXML
+	public void addComputationCell() {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/processmining/filterd/gui/fxml/ComputationCell.fxml"));
+			HBox cellLayout = (HBox) loader.load();
+			layout.getChildren().add(cellLayout);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/org/processmining/filterd/gui/NotebookController.java
+++ src/org/processmining/filterd/gui/NotebookController.java
@@ -1,11 +1,15 @@
 package org.processmining.filterd.gui;
 
+import java.io.IOException;
+
 import org.deckfour.xes.model.XLog;
 import org.processmining.contexts.uitopia.UIPluginContext;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class NotebookController {
@@ -31,5 +35,18 @@ public class NotebookController {
 	
 	public NotebookController(NotebookModel model) {
 		this.model = model;
+	}
+	
+	@FXML
+	public void addCell() {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/processmining/filterd/gui/fxml/AddCell.fxml"));
+			AddCellController addCellController = new AddCellController(model, layout);
+			loader.setController(addCellController);
+			HBox addCellLayout = (HBox) loader.load();
+			layout.getChildren().add(addCellLayout);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
