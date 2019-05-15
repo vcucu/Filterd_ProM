da2f2231eeeb38797610c53cac05e8610abc58f2
Diana Epureanu <epureanudiana13@gmail.com>
2019-05-15 14:42:29 +0200

    Started implementing FilterdTraceStartEventConfig and FilterdTraceStartEventCategoricalConfig classes

--- src/org/processmining/filterd/configurations/FilterdTraceStartEventCategoricalConfig.java
+++ src/org/processmining/filterd/configurations/FilterdTraceStartEventCategoricalConfig.java
@@ -1,17 +1,44 @@
 package org.processmining.filterd.configurations;
-import org.processmining.filterd.parameters.Parameter;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JComponent;
 
+import org.deckfour.xes.model.XEvent;
 import org.deckfour.xes.model.XLog;
+import org.deckfour.xes.model.XTrace;
 import org.processmining.filterd.filters.Filter;
+import org.processmining.filterd.parameters.Parameter;
+import org.processmining.filterd.parameters.ParameterMultipleFromSet;
+import org.processmining.filterd.parameters.ParameterYesNo;
 
 public class FilterdTraceStartEventCategoricalConfig extends FilterdAbstractConfig {
 
-	public FilterdTraceStartEventCategoricalConfig(XLog log, Filter filterType) {
+	public FilterdTraceStartEventCategoricalConfig(XLog log, Filter filterType, String attribute) {
 		super(log, filterType);
 		parameters = new ArrayList<Parameter>();
+		
+		// Create list of all values for the selected attribute
+		List<String> allValues = new ArrayList<>();
+		for (XTrace trace : log) {
+			for (XEvent event : trace) {
+				String value = event.getAttributes().get(attribute).toString();
+				if (!allValues.contains(value)) {
+					allValues.add(value);
+				}
+			}
+		}
+				
+		// Create desiredEvents parameter	
+		ParameterMultipleFromSet desiredEvents = new ParameterMultipleFromSet("desiredEvents",
+				"Select values", allValues, allValues);
+		
+		//Create nullHandling parameter
+		ParameterYesNo nullHandling = new ParameterYesNo("nullHandling", 
+				"Remove if no value provided", true);
+		
+		parameters.add(desiredEvents);
+		parameters.add(nullHandling);
 	}
 
 	public FilterdAbstractConfig populate(JComponent component) {
