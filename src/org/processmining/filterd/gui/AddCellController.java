6389824b5818539d3f1e1f856409889a40bc241d
Omar Alabbasi <omar.alabbasi97@gmail.com>
2019-05-15 15:05:47 +0200

    Fixed Notebook layout

--- src/org/processmining/filterd/gui/AddCellController.java
+++ src/org/processmining/filterd/gui/AddCellController.java
@@ -8,7 +8,6 @@ import org.processmining.contexts.uitopia.UIPluginContext;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class AddCellController {
@@ -30,7 +29,7 @@ public class AddCellController {
 	public void addComputationCell() {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/processmining/filterd/gui/fxml/ComputationCell.fxml"));
-			HBox cellLayout = (HBox) loader.load();
+			VBox cellLayout = (VBox) loader.load();
 			layout.getChildren().add(cellLayout);
 		} catch(IOException e) {
 			e.printStackTrace();
