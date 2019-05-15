2edf7df88d57985faefb7d94a8ec68eedbaea34d
florianmarkusse <florianmarkusse@hotmail.com>
2019-05-15 14:33:24 +0200

    Made value from range generic

--- src/org/processmining/filterd/configurations/FilterdTraceSampleConfig.java
+++ src/org/processmining/filterd/configurations/FilterdTraceSampleConfig.java
@@ -1,15 +1,26 @@
 package org.processmining.filterd.configurations;
-import org.processmining.filterd.parameters.Parameter;
 import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JComponent;
 
 import org.deckfour.xes.model.XLog;
 import org.processmining.filterd.filters.Filter;
+import org.processmining.filterd.parameters.ParameterValueFromRange;
 
 public class FilterdTraceSampleConfig extends FilterdAbstractConfig {
 
 	public FilterdTraceSampleConfig(XLog log, Filter filterType) {
 		super(log, filterType);
+		parameters = new ArrayList<>();
+		
+		List<Integer> optionsPair = new ArrayList<>();
+		optionsPair.add(0);
+		optionsPair.add(log.size());
+		
+		ParameterValueFromRange param = new ParameterValueFromRange<>("threshold", "Sample size", new Integer(0), optionsPair);
+		
+		parameters.add();
 		// TODO Auto-generated constructor stub
 	}
 
