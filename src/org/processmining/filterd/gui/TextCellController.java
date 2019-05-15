d6bf7afdd6ee2adfba723f6da5a2a89dbe8005ac
Filip Davidovic <filip.davidovic@protonmail.com>
2019-05-15 13:48:51 +0200

    added the basic version of text cell (xml and controller skeleton)

---
+++ src/org/processmining/filterd/gui/TextCellController.java
@@ -0,0 +1,14 @@
+package org.processmining.filterd.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class TextCellController {
+	@FXML private TextField textCellNameField;
+	@FXML private TextArea textCellTextArea;
+	
+	TextCellController() {
+		
+	}
+}
