83fbe94960037c0eff97512d875cbcd140d3677c
admin <paulchirita28@yahoo.com>
2019-05-15 13:19:16 +0200

    commented the trace sample filter

--- src/org/processmining/filterd/filters/FilterdTraceSampleFilter.java
+++ src/org/processmining/filterd/filters/FilterdTraceSampleFilter.java
@@ -10,7 +10,6 @@ import org.deckfour.xes.model.XAttributeMap;
 import org.deckfour.xes.model.XLog;
 import org.deckfour.xes.model.XTrace;
 import org.processmining.filterd.parameters.Parameter;
-import org.processmining.filterd.parameters.ParameterText;
 import org.processmining.filterd.parameters.ParameterValueFromRange;
 import org.processmining.framework.plugin.PluginContext;
 
@@ -18,9 +17,12 @@ public class FilterdTraceSampleFilter extends Filter {
 
 	public XLog filter(PluginContext context, XLog log, List<Parameter> parameters) {
 	
+		//get value of threshold parameter (i. e. the desired number of samples)
 		ParameterValueFromRange nrSamples = (ParameterValueFromRange) this.getParameter(parameters, "threshold");
+		
 		Random rand = new Random();
 		
+		//initialize the log that will be output
 		XFactory factory = XFactoryRegistry.instance().currentDefault();
 		XLog filteredLog = factory.createLog((XAttributeMap) log.getAttributes().clone());
 		filteredLog.getClassifiers().addAll(log.getClassifiers());
@@ -28,10 +30,16 @@ public class FilterdTraceSampleFilter extends Filter {
 		filteredLog.getGlobalTraceAttributes().addAll(log.getGlobalTraceAttributes());
 		filteredLog.getGlobalEventAttributes().addAll(log.getGlobalEventAttributes());
 		
+		//list of the randomly generated unique trace keys 
 		List<Integer> tracesAdded = new ArrayList<>(); 
 		
+		//while we haven't sampled enough traces
 		while(tracesAdded.size() < (int) nrSamples.getChosen()) {
+			//generate random trace key
 			int random = rand.nextInt(log.size());
+			
+			//if the trace hasn't been added before, add it to the output log and 
+			//add its key to the list of generated keys 	
 			if (!tracesAdded.contains(random)) {
 				tracesAdded.add(random);
 				XTrace sampleTrace = factory.createTrace(log.get(random).getAttributes());
