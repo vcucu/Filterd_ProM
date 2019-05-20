cd1812f33c1711f47478c6868d716be763b5ee47
admin <paulchirita28@yahoo.com>
2019-05-20 12:17:47 +0200

    trace frequency filter algorithm

--- src/org/processmining/filterd/filters/FilterdTraceFrequencyFilter.java
+++ src/org/processmining/filterd/filters/FilterdTraceFrequencyFilter.java
@@ -1,7 +1,10 @@
 package org.processmining.filterd.filters;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -9,6 +12,8 @@ import org.deckfour.xes.model.XEvent;
 import org.deckfour.xes.model.XLog;
 import org.deckfour.xes.model.XTrace;
 import org.processmining.filterd.parameters.Parameter;
+import org.processmining.filterd.parameters.ParameterMultipleFromSet;
+import org.processmining.filterd.parameters.ParameterOneFromSet;
 import org.processmining.filterd.parameters.ParameterRangeFromRange;
 import org.processmining.framework.plugin.PluginContext;
 
@@ -27,7 +32,7 @@ public class FilterdTraceFrequencyFilter extends Filter {
 		for (XTrace trace : clonedLog) {
 			List<String> activities = new ArrayList<String>();
 			for (XEvent event : trace) {
-				//activities.add(parameters.getClassifier().getClassIdentity(event));
+				activities.add(log.getClassifiers().get(0).getClassIdentity(event));
 			}
 			traceActivitiesMap.put(trace, activities);
 			if (traceOccurrenceMap.keySet().contains(activities)) {
@@ -36,8 +41,89 @@ public class FilterdTraceFrequencyFilter extends Filter {
 				traceOccurrenceMap.put(activities, 1);
 			}
 		}
+		List<Integer> occurrences = new ArrayList<Integer>(traceOccurrenceMap.values());
+		Collections.sort(occurrences);
 		
-		return null;
+		ParameterOneFromSet FreqOcc = (ParameterOneFromSet) parameters.get(0);
+		ParameterRangeFromRange<Integer> thresholdParameters = (ParameterRangeFromRange<Integer>) parameters.get(1);
+		ParameterOneFromSet filterInOut = (ParameterOneFromSet) parameters.get(2);
+		
+		
+		Collection<XTrace> tracesOutsideThreshold = new HashSet<XTrace>();
+		Collection<XTrace> tracesInsideThreshold = new HashSet<XTrace>();
+
+		int lowThreshold;
+		int highThreshold;
+		if (FreqOcc.getChosen().contains("occ")) {
+			lowThreshold = thresholdParameters.getOptionsPair().get(0);
+			highThreshold = thresholdParameters.getOptionsPair().get(1);
+		}
+		else {
+			lowThreshold = (thresholdParameters.getOptionsPair().get(0) * clonedLog.size()) / 100;
+			highThreshold = (thresholdParameters.getOptionsPair().get(0) * clonedLog.size()) / 100;
+			int sum = 0;
+			int index = -1;
+			while (sum < highThreshold) {
+				sum += occurrences.get(++index);
+			}
+			/*
+			 * The 'traces' occurrences[index]...occurrences[occurrences.size()-1]
+			 * cover more than X% if the log, where X = parameters.getThreshold().
+			 */
+			highThreshold = occurrences.get(index);
+			/*
+			 * If we take all traces that occur at least as many times as threshold
+			 * times, we cover at least X% of the log.
+			 */
+			if (highThreshold == occurrences.get(occurrences.size() - 1) + 1) {
+				/*
+				 * We're about to remove all traces. That seems to be undesirable.
+				 */
+				highThreshold--;
+			}
+			sum = 0;
+			index = -1;
+			while (sum < lowThreshold) {
+				sum += occurrences.get(++index);
+			}
+			/*
+			 * The low-frequency 'traces' (occurrences[0]...occurrences[index]) counted so far make up for X% of the log, 
+			 * where X = parameters.getThreshold().
+			 */
+			lowThreshold = (index == -1 ? 0 : occurrences.get(index)) + 1;
+			/*
+			 * If we take all traces that occur fewer than threshold times, we cover at least X% of the log.
+			 */
+			
+			if (lowThreshold == occurrences.get(occurrences.size() - 1) + 1) {
+				/*
+				 * We're about to remove all traces. That seems to be undesirable.
+				 */
+				lowThreshold--;
+			}
+		}
+		for (XTrace trace : clonedLog) {
+			/*
+			 * Trace is within thresholds set by user. Add to hashset.
+			 */
+			if (traceOccurrenceMap.get(traceActivitiesMap.get(trace)) < lowThreshold &&
+					traceOccurrenceMap.get(traceActivitiesMap.get(trace)) > highThreshold) {
+				tracesOutsideThreshold.add(trace);
+			} else {
+				tracesInsideThreshold.add(trace);
+			}
+		}
+		
+		if (filterInOut.getChosen().contains("in")) {
+			clonedLog.removeAll(tracesOutsideThreshold);
+		}
+		else {
+			clonedLog.removeAll(tracesInsideThreshold);
+		}
+		/*
+		 * tracesToRemove holds at least X% of the log.
+		 */
+		return clonedLog;
 	}
 
 }
