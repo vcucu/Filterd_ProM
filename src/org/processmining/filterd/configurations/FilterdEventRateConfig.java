68d7db982f4fbbf5b7908448b0007b0be07e643e
Diana Epureanu <epureanudiana13@gmail.com>
2019-05-20 09:12:55 +0200

    continued filter on event rate

--- src/org/processmining/filterd/configurations/FilterdEventRateConfig.java
+++ src/org/processmining/filterd/configurations/FilterdEventRateConfig.java
@@ -1,17 +1,40 @@
 package org.processmining.filterd.configurations;
-import org.processmining.filterd.parameters.Parameter;
 import java.util.ArrayList;
-import javax.swing.JComponent;
+import java.util.Arrays;
+import java.util.List;
 
 import org.deckfour.xes.model.XLog;
 import org.processmining.filterd.filters.Filter;
 import org.processmining.filterd.gui.FilterConfigPanelController;
+import org.processmining.filterd.parameters.Parameter;
+import org.processmining.filterd.parameters.ParameterOneFromSet;
+import org.processmining.filterd.parameters.ParameterValueFromRange;
+
 
 public class FilterdEventRateConfig extends FilterdAbstractConfig {
 
 	public FilterdEventRateConfig(XLog log, Filter filterType) {
 		super(log, filterType);
-		// TODO Auto-generated constructor stub
+		List<Parameter> parameters = new ArrayList<>();
+		
+		//Create the rate parameter
+		List<String> rateOptions = new ArrayList<>(Arrays.asList("Frequency","Occurence"));
+		ParameterOneFromSet rate = new ParameterOneFromSet("rate", "Choose rate type", rateOptions.get(0), rateOptions);
+		
+		//Create the threshold parameter
+		List<Integer> optionsPair = new ArrayList<>(Arrays.asList(0, 100));
+		ParameterValueFromRange<Integer> threshold = new ParameterValueFromRange<>("threshold",
+				"Select threshold for frequency/occurence", 0, optionsPair);
+		
+		//Create the selectionType parameter
+		List<String> selectionTypeOptions = new ArrayList<>(Arrays.asList("Filter in", "Filter out"));
+		ParameterOneFromSet selectionType = new ParameterOneFromSet("selectionType", "Selection type",
+				selectionTypeOptions.get(0), selectionTypeOptions);
+		
+		// Add all parameters to the list of parameters	
+		parameters.add(rate);
+		parameters.add(threshold);
+		parameters.add(selectionType);
 	}
 
 	public FilterdAbstractConfig populate(FilterConfigPanelController component) {
@@ -28,15 +51,10 @@ public class FilterdEventRateConfig extends FilterdAbstractConfig {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	public boolean checkValidity(XLog log) {
-		// TODO Auto-generated method stub
-		return false;
+	// I did not find any case where a different input log would cause invalidity
+	public boolean checkValidity(XLog candidateLog) {	
+		return true;
 	}
 
-	public XLog filter() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
