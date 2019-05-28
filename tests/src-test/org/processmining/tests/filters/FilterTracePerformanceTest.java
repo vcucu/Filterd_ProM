8d64a11c056e03db0114e2e3830b5014d7eeb7d8
florianmarkusse <florianmarkusse@hotmail.com>
2019-05-28 16:24:52 +0200

    changed directories from files and added 2 more test cases to performance filter

--- tests/src-test/org/processmining/tests/filters/FilterTracePerformanceTest.java
+++ tests/src-test/org/processmining/tests/filters/FilterTracePerformanceTest.java
@@ -22,8 +22,8 @@ public class FilterTracePerformanceTest extends FilterdPackageTest {
 	 * Result: cases 35, 41, 72.
 	 */
 	@Test
-	public void testPerformanceDuration() throws Throwable {
-		XLog expected = parseLog("trace-attribute", "test_performance_1.xes");
+	public void testPerformanceDuration1() throws Throwable {
+		XLog expected = parseLog("trace-performance", "test_duration_1.xes");
 		XLog computed = null; // insert filter operation
 		
 		FilterdTracePerformanceFilter filter = 
@@ -38,6 +38,29 @@ public class FilterTracePerformanceTest extends FilterdPackageTest {
 
 		assert equalLog(expected, computed);
 	}
+	
+	/* 
+	 * See Disco Performance - duration of min 0 - max 24 hours.
+	 * 
+	 * Result: cases 34, 56, 73, 74, 75, 76
+	 */
+	@Test
+	public void testPerformanceDuration2() throws Throwable {
+		XLog expected = parseLog("trace-performance", "test_duration_2.xes");
+		XLog computed = null; // insert filter operation
+		
+		FilterdTracePerformanceFilter filter = 
+				new FilterdTracePerformanceFilter();
+		
+		List<Parameter> parameters = getTestParameters(
+				"filter on duration", 
+				0, 
+				86400000);
+		
+		computed = filter.filter(null, originalLog, parameters);
+
+		assert equalLog(expected, computed);
+	}
 
 	/* Corresponds to test case 35 from test_specification.xlsx.
 	 * See Disco Endpoints - number of events: min 3 events - max 6 events.
@@ -46,8 +69,8 @@ public class FilterTracePerformanceTest extends FilterdPackageTest {
 	 * Result: cases 56, 74, 75, 76.
 	 */
 	@Test
-	public void testPerformanceEvents() throws Throwable {
-		XLog expected = parseLog("trace-attribute", "test_performance_2.xes");
+	public void testPerformanceEvents1() throws Throwable {
+		XLog expected = parseLog("trace-performance", "test_number_of_events_1.xes");
 		XLog computed = null; // insert filter operation
 		
 		FilterdTracePerformanceFilter filter = 
@@ -59,6 +82,30 @@ public class FilterTracePerformanceTest extends FilterdPackageTest {
 				6);
 		
 		computed = filter.filter(originalLog, parameters);
+
+		assert equalLog(expected, computed);
+	}
+	
+	/* 
+	 * See Disco Endpoints - number of events: min 2 events - max 2 events.
+	 * See ProM - Filter log by attributes = "Select traces from log".
+	 * 
+	 * Result: cases 34
+	 */
+	@Test
+	public void testPerformanceEvents2() throws Throwable {
+		XLog expected = parseLog("trace-performance", "test_number_of_events_2.xes");
+		XLog computed = null; // insert filter operation
+		
+		FilterdTracePerformanceFilter filter = 
+				new FilterdTracePerformanceFilter();
+		
+		List<Parameter> parameters = getTestParameters(
+				"filter on number of events", 
+				2, 
+				2);
+		
+		computed = filter.filter(null, originalLog, parameters);
 
 		assert equalLog(expected, computed);
 	}
