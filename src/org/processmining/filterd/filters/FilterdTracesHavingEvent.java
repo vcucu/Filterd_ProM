3aa6a1176104ba0b59bebbf51a5b98340c5cacfe
admin <paulchirita28@yahoo.com>
2019-05-29 11:13:30 +0200

    TracesHavingEvent

--- src/org/processmining/filterd/filters/FilterdTracesHavingEvent.java
+++ src/org/processmining/filterd/filters/FilterdTracesHavingEvent.java
@@ -1,5 +1,6 @@
 package org.processmining.filterd.filters;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.deckfour.xes.model.XAttributeMap;
@@ -9,6 +10,7 @@ import org.deckfour.xes.model.XTrace;
 import org.processmining.filterd.parameters.Parameter;
 import org.processmining.filterd.parameters.ParameterMultipleFromSet;
 import org.processmining.filterd.parameters.ParameterOneFromSet;
+import org.processmining.filterd.tools.Toolbox;
 
 public class FilterdTracesHavingEvent extends Filter {
 
@@ -22,13 +24,30 @@ public class FilterdTracesHavingEvent extends Filter {
 		ParameterMultipleFromSet attrValuesParam = (ParameterMultipleFromSet) parameters.get(1);
 		List<String> attrValues = attrValuesParam.getChosen();
 		
+		ParameterOneFromSet selectionTypeParam = (ParameterOneFromSet) parameters.get(2);
+		String selectionType = selectionTypeParam.getChosen();
+		List<XTrace> toRemove = new ArrayList<>();
+		
 		for (XTrace trace : clonedLog) {
-			boolean ok = true;
+			boolean remove = true;
 			for (XEvent event : trace) {
 				XAttributeMap eventAttributes = event.getAttributes();
-				
+				if (selectionType.equals("Forbidden") &&
+						Toolbox.satisfies(eventAttributes, attrType, attrValues)) {
+					toRemove.add(trace);
+					break;			
+				}
+				if (selectionType.equals("Mandatory") &&
+						Toolbox.satisfies(eventAttributes, attrType, attrValues)) {
+					remove = false;
+					break;
+				}
 	}
+			if (selectionType.equals("Mandatory") && remove) {
+				toRemove.add(trace);
+			}
 		}
+		clonedLog.removeAll(toRemove);
 		return clonedLog;
 }
 }
