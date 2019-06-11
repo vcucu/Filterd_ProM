175045d68c47ee2e6b2fc0b26c18e78dcb20517b
florianmarkusse <florianmarkusse@hotmail.com>
2019-06-11 11:28:17 +0200

    added config test for trace timeframe

--- tests/src-test/org/processmining/tests/config/ConfigTraceTimeframeTest.java
+++ tests/src-test/org/processmining/tests/config/ConfigTraceTimeframeTest.java
@@ -1,6 +1,5 @@
 package org.processmining.tests.config;
 
-import org.deckfour.xes.model.XLog;
 import org.junit.Test;
 import org.processmining.filterd.configurations.FilterdTraceTimeframeConfig;
 import org.processmining.filterd.filters.FilterdTraceTimeframeFilter;
@@ -12,30 +11,15 @@ public class ConfigTraceTimeframeTest extends FilterdPackageTest {
 		super();
 	}
 	
-	/* Testing correct working of can populate */
+	
+	/* Testing correct working of constructor */
 	@Test
-	public void testCanPopulate() throws Exception {
+	public void testConstructor() throws Exception {
 		try {
 			FilterdTraceTimeframeConfig config = new FilterdTraceTimeframeConfig(originalLog, new FilterdTraceTimeframeFilter());
 		} catch (Throwable exception) {
 			assertFalse(exception.equals(null));
 		}
-		// assertEquals(true, config.canPopulate(null));
-	}
-	
-	/* a log that is different should be invalid */
-	@Test
-	public void testInvalid() throws Exception {
-		XLog invalid = parseLog("trace-follower", "test_follower_directly_1.xes");
-		FilterdTraceTimeframeConfig config = new FilterdTraceTimeframeConfig(originalLog, new FilterdTraceTimeframeFilter());
-		assert !config.checkValidity(invalid);
-	}
-
-	/* a log that is the same should be valid */
-	@Test
-	public void testValid() throws Exception {
-		FilterdTraceTimeframeConfig config = new FilterdTraceTimeframeConfig(originalLog, new FilterdTraceTimeframeFilter());
-		assert config.checkValidity(originalLog);
 	}
 	
 }
