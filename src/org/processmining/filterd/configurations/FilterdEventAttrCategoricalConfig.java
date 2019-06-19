694433c17c4bd9337aa940c5034b4acf2b0d6a0d
florianmarkusse <florianmarkusse@hotmail.com>
2019-06-19 12:56:07 +0200

    added docs to event attribute categorical config

--- src/org/processmining/filterd/configurations/FilterdEventAttrCategoricalConfig.java
+++ src/org/processmining/filterd/configurations/FilterdEventAttrCategoricalConfig.java
@@ -14,20 +14,28 @@ import org.processmining.filterd.tools.Toolbox;
 
 public class FilterdEventAttrCategoricalConfig extends FilterdAbstractReferenceableConfig{
 
+	// List to hold the options.
 	List<String> optionList;
+	// List to hold the values
 	List<String> values;
+	// List to hold the default values.
 	List<String> defaultValues;
+	// parameter to select values.
 	ParameterMultipleFromSet desiredValues;
 	
+	// The key.
 	String key;
 	
 	
 	public FilterdEventAttrCategoricalConfig(XLog log, Filter filterType, String key) {
 		super(log, filterType);
+		// Set the key.
 		this.key = key;
+		// Initialize
 		parameters = new ArrayList<>();
 		optionList = new ArrayList<>();
 		
+		// filter in or filter out options.
 		optionList.add("Filter in");
 		optionList.add("Filter out");
 		
@@ -42,36 +50,52 @@ public class FilterdEventAttrCategoricalConfig extends FilterdAbstractReferencea
 		ParameterOneFromSet selectionType = new ParameterOneFromSet("selectionType",
 				"Select option for filtering", optionList.get(0), optionList);
 		
+		// Initialize values member.
 		values = new ArrayList<>();
+		// Loop over all traces in the log.
 		for (XTrace trace: log) {
+			// Loop over all events in the trace.
 			for (XEvent event : trace) {
 				if (!event.getAttributes().containsKey(key)) continue;
 				String value = event.getAttributes().get(key).toString();
+				// Add all values to the list.
 				if(!values.contains(value)) {
 					values.add(value);						
 				}
 			}
 		}
+		// Create default values from values list.
+		// list of 1 value, the first of the values list.
 		defaultValues = new ArrayList<>();
 		defaultValues.add(values.get(0));
 		
+		// Create parameter with all the values calculated.
 		desiredValues = new ParameterMultipleFromSet(
 				"desiredValues", "Choose values:", defaultValues, values);
 
+		// Add all parameters created to the parameters list.
 		parameters.add(selectionType);
 		parameters.add(desiredValues);
 		parameters.add(traceHandling);
 		parameters.add(eventHandling);
 	}
 
+	/**
+	 * Check if the parameters are still valid on the candidate log.
+	 * 
+	 * @param candidateLog the log to check.
+	 */
 	public boolean checkValidity(XLog log) {
         		
 		if(key == null) return true;
 		if(!Toolbox.computeAttributes(log).contains(key)) return false;
 		
+		// Loop over all traces in the log.
 		for (XTrace trace : log) {
+			// Loop over all events in the trace.
 			for (XEvent event : trace) {
 				if(!event.getAttributes().containsKey(key)) continue;
+				// If the new log has the same key and value that is selcted.
 				if(desiredValues.getChosen().contains(event.getAttributes().get(key).toString())) return true;
 			}
 		}
@@ -79,11 +103,19 @@ public class FilterdEventAttrCategoricalConfig extends FilterdAbstractReferencea
 		return false;	
 	}
 	
+	/**
+	 * Checks if the configuration can populate the parameters.
+	 * 
+	 * @param component The component that populates the parameters.
+	 */
 	public boolean canPopulate(FilterConfigPanelController component) {
-		//check whether no params are empty if you populate with the component
+		//check whether no params are empty if you populate with the component.
 		return true;
 	}
 	
+	/**
+	 * Getter for the key.
+	 */
 	public String getKey() {
 		return key;
 	}
