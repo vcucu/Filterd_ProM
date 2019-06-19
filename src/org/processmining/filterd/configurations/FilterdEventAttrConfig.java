47a4d79aa1b3c453cfc93488e127c7c451f874af
florianmarkusse <florianmarkusse@hotmail.com>
2019-06-19 11:32:48 +0200

    added docs for 2 files

--- src/org/processmining/filterd/configurations/FilterdEventAttrConfig.java
+++ src/org/processmining/filterd/configurations/FilterdEventAttrConfig.java
@@ -18,18 +18,24 @@ import org.processmining.filterd.widgets.ParameterOneFromSetExtendedController;
 
 public class FilterdEventAttrConfig extends FilterdAbstractReferencingConfig {
 
+	// Collection to hold the attributes of the events.
 	Collection<XAttribute> eventAttributes;
 
 	public FilterdEventAttrConfig(XLog log, Filter filterType) {
 		super(log, filterType);
+		// Create list for holding the parameters.
 		parameters = new ArrayList<Parameter>();
 
+		// Create list for holding the keys of the event attributes.
 		List<String> attributes = new ArrayList<String>();
 
+		// Get XLog info to get all the event attributes.
 		XLogInfo logInfo = XLogInfoImpl.create(log);
 		eventAttributes = logInfo.getEventAttributeInfo().getAttributes();
 
+		// Loop over all the event attribute keys.
 		for (String key : logInfo.getEventAttributeInfo().getAttributeKeys()) {
+			// Add to attributes list.
 			attributes.add(key);
 		}
 
@@ -37,7 +43,9 @@ public class FilterdEventAttrConfig extends FilterdAbstractReferencingConfig {
 		ParameterOneFromSet attribute = new ParameterOneFromSet("attribute", 
 				"Filter by", attributes.get(0), attributes, true);
 
+		// Add to parameter list.
 		parameters.add(attribute);
+		// Switch reference based on the chosen attribute.
 		switchReference(attributes.get(0));
 	}
 
@@ -48,45 +56,73 @@ public class FilterdEventAttrConfig extends FilterdAbstractReferencingConfig {
 
 	@Override
 	public AbstractFilterConfigPanelController getConfigPanel() {
+		// If the configuration panel is not initialized yet.
 		if (this.configPanel == null) {
+			// Create it.
 			this.configPanel = new FilterConfigPanelController("Event Attribute Configuration", parameters, this);
 		}
 		
 		return configPanel;
 	}
 	
+	/**
+	 * Changes the reference this configuration is holding.
+	 * 
+	 * @param chosen The controller on which the reference depends.
+	 * 
+	 * @return The correct reference.
+	 */
 	public FilterdAbstractConfig changeReference(ParameterOneFromSetExtendedController chosen) {
+		// Remove all parameters that belong to the concrete reference.
 		for (Parameter param : concreteReference.getParameters()) {
 			parameters.remove(param);
 		}
+		// Switch based on the chosen value.
 		switchReference(chosen.getValue());
+		// Populate the parameters list with the parameters of the new 
+		// reference.
 		for (Parameter param : concreteReference.getParameters()) {
 			parameters.add(param);
 		}
 		return concreteReference;
 	}
 
+	/**
+	 * Switches the concrete reference based on the key.
+	 * 
+	 * @param key To switch on.
+	 */
 	public void switchReference(String key) {
+		// Loop over all event attributes.
 		for (XAttribute a : eventAttributes) {
+			// Get the string representation of the key.
 			String k = a.getKey();
+			// If this equals the key.
 			if (k.equals(key)) {
+				// Switch based on the type.
 				switch(Toolbox.getType(a)) {
 					case "Literal":
+						// Literal, thus we do categorical.
 						concreteReference = new FilterdEventAttrCategoricalConfig(log, filterType, k);
 						break;
 					case "Boolean":
+						// Boolean, thus we do categorical.
 						concreteReference = new FilterdEventAttrCategoricalConfig(log, filterType, k);
 						break;
 					case "Continuous":
+						// Continuous, thus we do numerical.
 						concreteReference = new FilterdEventAttrNumericalConfig(log, filterType, k);
 						break;
 					case "Discrete":
+						// Discrete, thus we do numerical.
 						concreteReference = new FilterdEventAttrNumericalConfig(log, filterType, k);
 						break;
 					case "ID":
+						// ID, thus we do categorical.
 						concreteReference = new FilterdEventAttrCategoricalConfig(log, filterType, k);
 						break;
 					case "Timestamp":
+						// Timestamp, thus we do date.
 						concreteReference = new FilterdEventAttrDateConfig(log, filterType);
 						break;
 					default: concreteReference = new FilterdEventAttrCategoricalConfig(log, filterType, k);
@@ -96,6 +132,11 @@ public class FilterdEventAttrConfig extends FilterdAbstractReferencingConfig {
 		}
 	}
 
+	/**
+	 * Check if the parameters are still valid on the candidate log.
+	 * 
+	 * @param log the log to check.
+	 */
 	public boolean checkValidity(XLog log) {
 		if (concreteReference == null) return true;
 		return concreteReference.checkValidity(log);
