43c0fa776198467b3bca46e670a8e2118ed0f144
florianmarkusse <florianmarkusse@hotmail.com>
2019-06-19 12:33:00 +0200

    added docs to abstract referencing config adapted

--- src/org/processmining/filterd/gui/adapters/FilterdAbstractReferencingConfigAdapted.java
+++ src/org/processmining/filterd/gui/adapters/FilterdAbstractReferencingConfigAdapted.java
@@ -8,13 +8,24 @@ import org.processmining.filterd.configurations.FilterdAbstractReferenceableConf
 @XmlRootElement
 public class FilterdAbstractReferencingConfigAdapted extends FilterdAbstractConfigAdapted {
 
+	// Reference to the referenceable configuration.
 	FilterdAbstractReferenceableConfig concreteReference;
 	
 	@XmlJavaTypeAdapter(FilterdAbstractConfigReferenceableAdapter.class)
+	/**
+	 * Getter to return the concrete reference.
+	 * 
+	 * @return concreteReference.
+	 */
 	public FilterdAbstractReferenceableConfig getConcreteReference() {
 		return concreteReference;
 	}
 	
+	/**
+	 * Setter for the concrete reference.
+	 * 
+	 * @param concreteReference the concrete reference to set it to.
+	 */
 	public void setConcreteReference(FilterdAbstractReferenceableConfig concreteReference) {
 		this.concreteReference = concreteReference;
 	}
